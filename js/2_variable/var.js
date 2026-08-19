84811573a7e647f07733ef4e54d90f098d93e4bc
mahek dobariya <mahekdobariya45@gmail.com>
2026-08-19 18:52:50 +0530

    js updated file

---
+++ js/2_variable/var.js
@@ -0,0 +1,14 @@
+// var x=10;
+// x=15;
+// document.write(`ans:-${x}`)
+
+// let city=null;
+// let city="pune"//error
+//document.write(`ans:-${city}`)
+
+// const pi=3.14;
+// console.log(`ans:-${pi}`)
+
+
+//console.log(typeof(city))
+
